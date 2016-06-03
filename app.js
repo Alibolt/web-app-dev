55fb6a31ddf1d6e4500e088ec215790d98e7782c
Karim Alibhai <karim@alibhai.co>
2016-06-03 16:12:25 -0400

    misc updates

---
+++ app.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const express = require('express'),
+      app = express(),
+      redirect = express(),
+      smtp = require('nodemailer-smtp-transport'),
+      transport = require('nodemailer').createTransport(smtp({
+          host: 'smtp.gmail.com',
+          auth: {
+              user: 'karim@alibhai.co',
+              pass: 'prmcbrtqyrhemrpu'
+          }
+      })),
+      fs = require('fs'),
+      path = require('path'),
+      qs = require('querystring'),
+      https = require('http2').createServer({
+          key: fs.readFileSync(path.resolve(__dirname, 'ssl', 'web-apps.co.key'), 'utf8'),
+          cert: fs.readFileSync(path.resolve(__dirname, 'ssl', '1_web-apps.co_bundle.crt'), 'utf8'),
+          ca: fs.readFileSync(path.resolve(__dirname, 'ssl', 'ca.pem'), 'utf8')
+      }, function (req, res) {
+          const url = require('url').parse(req.url);
+          let file = url.pathname;
+          file = file === '/' ? '/index.html' : file;
+          
+          if (file === '/post') {
+              const data = qs.parse(url.query);
+
+              transport.sendMail({
+                  subject: 'New Question',
+                  to: 'web-apps@apps.alibhai.co',
+                  from: 'questions@web-apps.co',
+                  text: `A user from web-apps.co has asked:\n\n"${data.question}"`,
+                  html: `<i>A user from web-apps.co has asked:</i><br><br><blockquote>${data.question}</blockquote>`
+              }, (err) => {
+                  if (err) {
+                      console.log(err);
+                      res.statusCode = 500;
+                  }
+
+                  res.end();
+              });
+          } else {
+              const fd = path.resolve(__dirname, '.' + file);
+
+              if (fs.existsSync(fd)) {
+                  fs.createReadStream(fd).pipe(res);
+              } else {
+                  res.statusCode = 404;
+                  res.end();
+              }
+          };
+      }),
+      http = require('http').createServer(redirect);
+
+redirect.use((req, res) => res.redirect(301, `https://web-apps.co${req.originalUrl}`));
+
+http.listen(80, () => console.log('HTTP redirect setup on :80'))
+https.listen(443, () => console.log('listening on :443'));
--- js/index.js
+++ js/index.js
@@ -2,8 +2,120 @@
     'use strict';
     
     var duration = 7000, steps = 3, step = 1;
+    var stopwords = ['is', 'am', 'are', 'was', 'were', 'the', 'and', 'but', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '$', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
-	setInterval(function () {
-		document.querySelector( '.splash' ).setAttribute( 'data-animation-step', step = ++step > steps ? 1 : step );
-	}, duration / steps);
+    setInterval(function () {
+        document.querySelector( '.splash' ).setAttribute( 'data-animation-step', step = ++step > steps ? 1 : step );
+    }, duration / steps);
+
+    app.controller('NavController', function ($scope) {
+        $scope.query = '';
+        $scope.questions = {};
+        $scope.alertShown = false;
+
+        $scope.closeNav = function () {
+            $('#navbar').collapse('hide');
+        };
+
+        $.getJSON('/faq.json')
+         .done(function (data) {
+             $scope.$apply(function () { $scope.results = $scope.questions = data });
+
+             $('.collapse').collapse({ toggle: false }).on('click', function () { $('.collapse.in').collapse() });
+             $('.panel-title a').on('click', function () { $(this).blur() });
+             if (window.screen.width > 610) $('[data-toggle="tooltip"]').tooltip();
+         })
+         .fail(function (err) { console.error(err) });
+
+        $scope.results = {};
+        $scope.updateResults = function () {
+            var results = {}, query = $scope.query.split(/\s+/g).filter(function (word) {
+                return word;
+            });
+            
+            if (query.length === 0) {
+                $scope.results = $scope.questions;
+                return;
+            }
+
+            Object.keys($scope.questions)
+                  .forEach(function (question) {
+                      var i, j;
+                      var keywords = question.split(' ').filter(function (word) {
+                          return word && stopwords.indexOf(word) === -1;
+                      });
+
+                      console.log(question);
+                      for (i = 0; i < query.length; i += 1) {
+                          for (j = 0; j < keywords.length; j += 1) {
+                              console.log('%s.indexOf(%s) = %s', keywords[j], query[i], keywords[j].indexOf(query[i]));
+                              if (keywords[j].indexOf(query[i]) === 0) {
+                                  results[question] = $scope.questions[question];
+                                  return;
+                              }
+                          }
+                      }
+                  });
+
+            $scope.results = results;
+            
+            $('.collapse').collapse({ toggle: false }).on('click', function () { $('.collapse.in').collapse() });
+            $('.panel-title a').on('click', function () { $(this).blur() });
+            if (window.screen.width > 610) $('[data-toggle="tooltip"]').tooltip();
+        };
+
+        $scope.simplify = function (question) {
+            return question.replace(/[^a-z]+/gi, '');
+        };
+
+        $scope.hasQuestions = function () {
+            for (var i in $scope.questions) {
+                return true;
+            }
+            
+            return false;
+        };
+
+        $scope.noResults = function () {
+            for (var i in $scope.results) {
+                return false;
+            }
+            
+            return true;
+        };
+        
+        $scope.ask = function () {
+            $scope.hideAlert();
+            $scope.newFAQ = ($scope.newFAQ || '').trim();
+            
+            if ($scope.newFAQ) {
+                $.ajax({
+                    url: '/post?question=' + encodeURIComponent($scope.newFAQ),
+                    success: function () {
+                        $scope.alertShown = true;
+                        $scope.$apply();
+                    },
+                    error: function () {
+                        $scope.errorShown = true;
+                        $scope.$apply();
+                    }
+                });
+
+                $scope.newFAQ = '';
+            }
+        };
+
+        $scope.hideAlert = function () {
+            $scope.error = '';
+            $scope.alertShown = $scope.errorShown = false;
+        };
+
+        $('.search-input,.search-form .form-control').on('focus', function () {
+            $('html').addClass('search');
+        });
+
+        $('.search-input,.search-form .form-control').on('blur', function () {
+            $('html').removeClass('search');
+        });
+    });
 }(angular.module('webapps', ['ngAnimate']));
