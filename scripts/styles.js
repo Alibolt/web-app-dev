6fbfe276c4d70053b9e6771214a96761a9eb8fcc
Karim Alibhai <karim@alibhai.co>
2016-06-28 21:39:51 -0400

    log styles as they compile

--- scripts/styles.js
+++ scripts/styles.js
@@ -3,7 +3,7 @@
  * Uses Swirl (https://github.com/karimsa/swirl).
  */
 
-window.style=(function(s,w,i,r,l){'use strict';w=new Style;w.tag.parentElement.removeChild(w.tag),document.body.appendChild(w.tag);for(i in s)if(s.hasOwnProperty(i)){w.attach(r=(new Rule).apply(i));for(l in s[i])s[i].hasOwnProperty(l)&&r[l](s[i][l])};return w}({
+window.style=(function(s,w,i,r,l){'use strict';w=new Style;w.tag.parentElement.removeChild(w.tag),document.body.appendChild(w.tag);for(i in s)if(s.hasOwnProperty(i)){w.attach(r=(new Rule).apply(i));console.log(`creating rule for ${i}`);for(l in s[i])s[i].hasOwnProperty(l)&&r[l](s[i][l])};return w}({
     '.pull-right': {
         float: 'right'
     },
